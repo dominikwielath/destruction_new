8c98f83f3d4534794646a29b7527bea64f9adf5f
dominik.wielath <dominik.wielath@gmail.com>
2024-01-08 19:19:41 +0100

    moschun rest predicitons

--- predict.py
+++ predict.py
@@ -22,9 +22,9 @@ python -m predict 3 aleppo,daraa
 
 # For artemisa
 # CITIES = ['hostomel', 'irpin', 'kharkiv', 'livoberezhnyi', 'moschun', 'rubizhne', 'volnovakha', 'aleppo', 'damascus', 'daraa', 'deirezzor','hama', 'homs', 'idlib', 'raqqa']
-CITIES = ['irpin', 'kharkiv', 'livoberezhnyi', 'moschun', 'rubizhne', 'volnovakha', 'aleppo', 'damascus', 'daraa', 'deirezzor','hama', 'homs', 'idlib', 'raqqa']
+CITIES = ['hostomel', 'irpin', 'kharkiv', 'livoberezhnyi', 'rubizhne', 'volnovakha', 'aleppo', 'damascus', 'daraa', 'deirezzor','hama', 'homs', 'idlib', 'raqqa']
 
-OUTPUT_DIR = "/lustre/ific.uv.es/ml/iae091/outputs/runs/irpin-kharkiv-livoberezhnyi-moschun-rubizhne-volnovakha-aleppo-damascus-daraa-deirezzor-hama-homs-idlib-raqqa_1"
+OUTPUT_DIR = "/lustre/ific.uv.es/ml/iae091/outputs/runs/hostomel-irpin-kharkiv-livoberezhnyi-rubizhne-volnovakha-aleppo-damascus-daraa-deirezzor-hama-homs-idlib-raqqa_1"
 DATA_DIR = "/lustre/ific.uv.es/ml/iae091/data"
 
 # ## For workstation
