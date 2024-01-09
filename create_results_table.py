d88503f4fd1fd42fb8650bee40f88685c4f86b53
dominik.wielath <dominik.wielath@gmail.com>
2024-01-09 09:15:52 +0100

    folder update

--- create_results_table.py
+++ create_results_table.py
@@ -3,7 +3,7 @@ import os
 import re
 import pandas as pd
 
-runs_dir = "/lustre/ific.uv.es/ml/iae091/outputs/runs/hostomel-irpin-kharkiv-livoberezhnyi-rubizhne-volnovakha-aleppo-damascus-daraa-deirezzor-hama-homs-idlib-raqqa_1/"
+runs_dir = "/lustre/ific.uv.es/ml/iae091/outputs/runs/irpin-kharkiv-livoberezhnyi-moschun-rubizhne-volnovakha-aleppo-damascus-daraa-deirezzor-hama-homs-idlib-raqqa_1/"
 #runs_dir = "/home/dominik/Documents/ra_hannes/war-destruction/artemisa-results/hostomel-irpin-kharkiv-livoberezhnyi-rubizhne-volnovakha-aleppo-damascus-daraa-deirezzor-hama-homs-idlib-raqqa_1"
 
 # Get the directory in which the runs are saved as input
